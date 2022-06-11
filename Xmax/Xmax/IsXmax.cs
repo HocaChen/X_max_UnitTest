4af35e1161ffd1b1e9161db75e052f954b2e876a
Hoca Chen <hocachen@gmail.com>
2022-06-11 23:26:19 +0800

    add logic

---
+++ Xmax/Xmax/IsXmax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Xmax
+{
+    public class IsXmax
+    {
+        static void Main()
+        {
+            var today = DateTime.Today;
+            if(today.Month == 12 && today.Day == 25)
+                Console.WriteLine("Today is Xmax");
+            else
+                Console.WriteLine("Today is not Xmax");
+        }
+    }
+}
