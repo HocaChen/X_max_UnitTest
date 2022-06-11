9c8afba552accb7181db1dc71edc4242fbd660ab
Hoca Chen <hocachen@gmail.com>
2022-06-11 23:12:47 +0800

    commit file
    commit file

---
+++ Xmax/Xmax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xmax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
